f1c3abccf9bf370675f1478afe6abe30c692a2f5
Weijie Yu <frank123111@gmail.com>
2018-04-08 01:37:40 -0700

    Intial commit

--- yolo_model_keras.py
+++ yolo_model_keras.py
@@ -1,9 +1,11 @@
 '''YOLO_v2 Model Defined in Keras'''
 import numpy as np
 import tensorflow as tf
+from keras import backend as K
 from keras.layers import Lambda
 from keras.layers.merge import concatenate
 from keras.models import Model
+from .yolo_darknet import compose, darknet_conv2d, darknet_conv2d_bn_leaky, darknet_body
 
 voc_anchors = np.array([[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], 16.62, 10.52])
 voc_classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
@@ -25,3 +27,86 @@ def space_to_depth_x2_output_shape(input_shape):
             input_shape[3]) if input_shape[1] else (input_shape[0], None, None,
                                                     4 * input_shape[3])
 
+def yolo_body(inputs, num_anchors, num_classes):
+    """Create YOLO_V2 model CNN body in Keras."""
+    outputs = darknet_body()(inputs)
+    darknet_model = Model(inputs, outputs)
+    conv20 = compose(darknet_conv2d_bn_leaky(1024, (3, 3)),
+                     darknet_conv2d_bn_leaky(1024, (3, 3)))(darknet_model.output)
+
+    conv13 = darknet_model.layers[43].output
+    conv21 = darknet_conv2d_bn_leaky(64, (1, 1))(conv13)
+    conv21_reshaped = Lambda(space_to_depth_x2,
+                             output_shape=space_to_depth_x2_output_shape,
+                             name='space_to_depth')(conv21) # Wraps the expression as a layer object
+    x = concatenate([conv21_reshaped, conv20])
+    x = darknet_conv2d_bn_leaky(1024, (3, 3))(x)
+    x = darknet_conv2d(num_anchors * (num_classes + 5), (1, 1))(x)
+    print("yolo output shape", x.shape)
+    return Model(inputs, x)
+
+def yolo_head(feats, anchors, num_classes):
+    """
+    Convert final layer features to bounding box parameters.
+
+    Parameters
+    ----------
+    feats : tensor
+        Final convolutional layer features.
+    anchors : array-like
+        Anchor box widths and heights.
+    num_classes : int
+        Number of target classes.
+
+    Returns
+    -------
+    box_xy : tensor
+        x, y box predictions adjusted by spatial location in conv layer.
+    box_wh : tensor
+        w, h box predictions adjusted by anchors and conv spatial resolution.
+    box_conf : tensor
+        Probability estimate for whether each box contains any object.
+    box_class_pred : tensor
+        Probability distribution estimate for each box over class labels.
+    """
+    num_anchors = len(anchors)
+    # Reshape to batch, height, width, num_anchors, box_params.
+    anchors_tensor = K.reshape(K.variable(anchors), [1, 1, 1, num_anchors, 2])
+    # Static implementation for fixed models.
+    # _, conv_height, conv_width, _ = K.int_shape(feats)
+    # conv_dims = K.variable([conv_width, conv_height])
+
+    # Dynamic implementation of conv dims for fully convolutional model.
+    conv_dims = K.shape(feats)[1:3]  # assuming channels last, exclude m batch number
+    # In YOLO the height index is the inner most iteration.
+    conv_height_index = K.arange(0, stop=conv_dims[0])
+    conv_width_index = K.arange(0, stop=conv_dims[1])
+    conv_height_index = K.tile(conv_height_index, [conv_dims[1]])
+    conv_width_index = K.tile(K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
+    conv_width_index = K.flatten(K.transpose(conv_width_index))
+    conv_index = K.transpose(K.stack([conv_height_index, conv_width_index]))
+    conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
+    conv_index = K.cast(conv_index, K.dtype(feats))
+
+    feats = K.reshape(feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
+    conv_dims = K.cast(K.reshape(conv_dims, [1, 1, 1, 1, 2]), K.dtype(feats))
+
+    # Static generation of conv_index:
+    # conv_index = np.array([_ for _ in np.ndindex(conv_width, conv_height)])
+    # conv_index = conv_index[:, [1, 0]]  # swap columns for YOLO ordering.
+    # conv_index = K.variable(
+    #     conv_index.reshape(1, conv_height, conv_width, 1, 2))
+    # feats = Reshape(
+    #     (conv_dims[0], conv_dims[1], num_anchors, num_classes + 5))(feats)
+
+    box_confidence = K.sigmoid(feats[..., 4:5])
+    box_xy = K.sigmoid(feats[..., :2])
+    box_wh = K.exp(feats[..., 2:4])
+    box_class_probs = K.softmax(feats[..., 5:])
+
+    # Adjust preditions to each spatial grid point and anchor size.
+    # Note: YOLO iterates over height index before width index.
+    box_xy = (box_xy + conv_index) / conv_dims
+    box_wh = box_wh * anchors_tensor / conv_dims
+
+    return box_confidence, box_xy, box_wh, box_class_probs
